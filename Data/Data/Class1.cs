8516f55c9cbb17f64e4ab2ba12327a3bc604165d
Zychu10 <s.zych10@o2.pl>
2022-04-21 19:19:18 +0200

    Project structure

---
+++ Data/Data/Class1.cs
@@ -0,0 +1,16 @@
+﻿namespace Data
+{
+    public class Ball
+    {
+        public int Radius { get; set; }
+        public int X { get; set; }
+        public int Y { get; set; }
+
+        public Ball(int r, int x, int y)
+        {
+            Radius = r;
+            X = x;
+            Y = y;
+        }
+    }
+}
