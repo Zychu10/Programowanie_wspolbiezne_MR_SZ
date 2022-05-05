52291d2899c6618ac9b234dacf9b1d6b32e76b6b
Zychu10 <s.zych10@o2.pl>
2022-05-05 19:49:27 +0200

    Everything without View and tests

--- Project/Project/ViewModel/MainWindowViewModel.cs
+++ Project/Project/ViewModel/MainWindowViewModel.cs
@@ -1,9 +1,58 @@
-﻿using System;
+﻿using Model;
+using System.Windows.Controls;
+using System.Windows.Input;
 
 namespace ViewModel
 {
-    public class MainWindowViewModel
+
+    public class MainWindowViewModel : BaseViewModel
     {
+        private readonly ModelAbstractAPI ModelLayer;
+        private int _BallVal;
+        public ICommand AddCommand { get; set; }
+
+        public MainWindowViewModel()
+        {
+
+            ModelLayer = ModelAbstractAPI.CreateApi(600, 480);
+            StopCommand = new RelayCommand(Stop);
+            AddCommand = new RelayCommand(CreateEllipses);
+
+        }
+
+
+        public ICommand StopCommand
+        { get; set; }
+
+
+        public int BallVal
+        {
+            get { return _BallVal; }
+            set
+            {
+                _BallVal = value;
+                RaisePropertyChanged();
+            }
+        }
+
+        public Canvas Canvas
+        {
+            get => ModelLayer.Canvas;
+
+        }
+
+        private void CreateEllipses()
+        {
+            ModelLayer.CreateEllipses(BallVal);
+        }
+        private void Stop()
+        {
+
+            ModelLayer.Stop();
+
+        }
+
+
 
     }
 }
