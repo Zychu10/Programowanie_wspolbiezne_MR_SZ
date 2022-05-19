da454bad66d215a81b1caf27b78fff6c5cc9a109
Zychu10 <s.zych10@o2.pl>
2022-05-19 20:14:02 +0200

    Data layer clearly stated, logic api rework

--- Project/Data/Ball.cs
+++ Project/Data/Ball.cs
@@ -8,12 +8,13 @@ namespace Data
 {
    public interface IBall : INotifyPropertyChanged
     {
-        int Size { get; }
-        double Weight { get; }
-        double X { get; set; }
-        double Y { get; set; }
-        double NewX { get; set; }
-        double NewY { get; set; }
+        int id { get; }
+        int size { get; }
+        double weight { get; }
+        double x { get; set; }
+        double y { get; set; }
+        double newX{ get; set; }
+        double newY{ get; set; }
         void Move();
         void CreateTask(int interval);
         void Stop();
@@ -21,6 +22,7 @@ namespace Data
     
     internal class Ball : IBall
     {
+        private readonly int _id;
         private readonly int _size;
         private double _x;
         private double _y;
@@ -31,8 +33,9 @@ namespace Data
         private Task task;
         private bool _stop = false;
 
-        public Ball(int size, double x, double y, double newX, double newY, double weight)
+        public Ball(int id, int size, double x, double y, double newX, double newY, double weight)
         {
+            _id = id;
             _size = size;
             _x = x;
             _y = y;
@@ -41,7 +44,91 @@ namespace Data
             _weight = weight;
         }
 
-        public int Size { get { return _size; } }
-
+        public int size { get { return _size; } }
+        public int id { get { return _id; } }
+        public double newX
+        {
+            get { return _newX; }
+            set
+            {
+                if (!value.Equals(_newX))
+                {
+                    _newX = value;
+                }
+                return;
+            }
+            
+        }
+        public double newY
+        {
+            get { return _newY; }
+            set
+            {
+                if (!value.Equals(_newY))
+                {
+                    _newY= value;
+                }
+                return;
+            }
+        }
+        public double x
+        {
+            get { return _x; }
+            set
+            {
+                if (!value.Equals(_x))
+                {
+                    _x = value;
+                    RaisePropertyChanged();
+                }
+                return;
+            }
+        }
+        public double y
+        {
+            get { return _y; }
+            set
+            {
+                if (!value.Equals(_y))
+                {
+                    _y = value;
+                    RaisePropertyChanged();
+                }
+            }
+        }
+        public void Move()
+        {
+            x += newX;
+            y += newY;
+        }
+        public double weight { get { return _weight; } }
+        public event PropertyChangedEventHandler PropertyChanged;
+        internal void RaisePropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+        public void CreateTask(int interval)
+        {
+            task = Go(interval);
+            _stop = false;
+        }
+        private async Task Go(int interval)
+        {
+            while(_stop == false)
+            {
+                stopwatch.Reset();
+                stopwatch.Start();
+                if(_stop == false)
+                {
+                    Move();
+                }
+                stopwatch.Stop();
+                await Task.Delay((int)(interval - stopwatch.ElapsedMilliseconds));
+            }
+        }
+        public void Stop()
+        {
+            _stop = true;
+        }
     }
 }
--- Project/Data/DataAPI.cs
+++ Project/Data/DataAPI.cs
@@ -1,17 +1,76 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.ObjectModel;
+using System.Threading;
 
 namespace Data
 {
     public abstract class DataAPI
     {
-        public static DataAPI CreateApi()
+        public abstract int CountBalls { get; }
+        public abstract IList CreateBallsList(int count);
+        public abstract int Width { get; }
+        public abstract int Height { get; }
+
+        public abstract IBall GetBall(int index);
+        public static DataAPI CreateApi(int width, int height)
         {
-            return new DataAPIBase();
+            return new DataAPIBase(width,height);
         }
     }
 
     internal class DataAPIBase : DataAPI
     {
+        private ObservableCollection<IBall> _balls { get;}
+        private readonly Mutex _mutex = new Mutex();
+        private readonly Random _random = new Random();
+        public override int Width { get; }
+        public override int Height { get; }
 
+        public DataAPIBase(int width, int height)
+        {
+            _balls = new ObservableCollection<IBall>();
+            Width = width;
+            Height = height;
+        }
+        public ObservableCollection<IBall> Balls { get { return _balls; } }
+        public override IList CreateBallsList(int count)
+        {
+           if(count > 0)
+            {
+                int ballsCounter = _balls.Count;
+                for(int i = 0; i < count; i++)
+                {
+                    _mutex.WaitOne();
+                    int radius = 20;
+                    double weight = 30;
+                    double x = _random.Next(radius, Width - radius);
+                    double y = _random.Next(radius, Height - radius);
+                    double newX = _random.Next(-10, 10);
+                    double newY = _random.Next(-10, 10);
+                    Ball ball = new Ball(i + 1 + ballsCounter, radius, x, y, newX, newY, weight);
+                    _balls.Add(ball);
+                    _mutex.ReleaseMutex();
+                }
+            }
+           if (count < 0)
+            {
+                for(int i = 0; i < 0; i++)
+                {
+                    if(_balls.Count > 0)
+                    {
+                        _mutex.WaitOne();
+                        _balls.Remove(_balls[_balls.Count - 1]);
+                        _mutex.ReleaseMutex();
+                    };
+                }
+            }
+           return _balls;
+        }
+        public override int CountBalls { get { return _balls.Count; } }
+        public override IBall GetBall(int index)
+        {
+            return _balls[index];
+        }
     }
 }
