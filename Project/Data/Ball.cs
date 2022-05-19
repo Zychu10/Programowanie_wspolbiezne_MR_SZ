236a48c5df143529e816750fb4116e301e167d47
Zychu10 <s.zych10@o2.pl>
2022-05-19 04:33:51 +0200

    Project Canvas rework, creating balls in Data Layer

---
+++ Project/Data/Ball.cs
@@ -0,0 +1,47 @@
+﻿using System.ComponentModel;
+using System.Diagnostics;
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
+
+
+namespace Data
+{
+   public interface IBall : INotifyPropertyChanged
+    {
+        int Size { get; }
+        double Weight { get; }
+        double X { get; set; }
+        double Y { get; set; }
+        double NewX { get; set; }
+        double NewY { get; set; }
+        void Move();
+        void CreateTask(int interval);
+        void Stop();
+    }
+    
+    internal class Ball : IBall
+    {
+        private readonly int _size;
+        private double _x;
+        private double _y;
+        private double _newX;
+        private double _newY;
+        private readonly double _weight;
+        private readonly Stopwatch stopwatch = new Stopwatch();
+        private Task task;
+        private bool _stop = false;
+
+        public Ball(int size, double x, double y, double newX, double newY, double weight)
+        {
+            _size = size;
+            _x = x;
+            _y = y;
+            _newX = newX;
+            _newY = newY;
+            _weight = weight;
+        }
+
+        public int Size { get { return _size; } }
+
+    }
+}
--- Project/Model/ModelAbstractAPI.cs
+++ Project/Model/ModelAbstractAPI.cs
@@ -11,11 +11,6 @@ namespace Model
     {
         public abstract int width { get; }
         public abstract int height { get; }
-        public abstract Canvas Canvas { get; set; }
-        public abstract List<Ellipse> ellipseCollection { get; }
-        public abstract void CreateEllipses(int ballVal);
-        public abstract void Move();
-
         public abstract void Stop();
 
 
@@ -30,55 +25,16 @@ namespace Model
         public override int height { get; }
 
         private LogicAbstractAPI LogicLayer;
-        public override List<Ellipse> ellipseCollection { get; }
-        public override Canvas Canvas { get; set; }
         public ModelApi(int Width, int Height)
         {
 
             width = Width;
             height = Height;
             LogicLayer = LogicAbstractAPI.CreateApi(width, height);
-            ellipseCollection = new List<Ellipse>();
-            Canvas = new Canvas();
-            Canvas.HorizontalAlignment = HorizontalAlignment.Center;
-            Canvas.VerticalAlignment = VerticalAlignment.Top;
-            Canvas.Margin = new Thickness(0, 134, 0, 0);
-            Canvas.Width = width;
-            Canvas.Height = height;
-            Canvas.Background = new SolidColorBrush(Color.FromRgb(245, 250, 205));
-            Canvas.Margin = new Thickness(0, 134, 0, 0);
-            Canvas.Background = new SolidColorBrush(Color.FromRgb(245, 250, 245));
-            LogicLayer.Update += (sender, args) => Move();
+          
         }
-        public override void CreateEllipses(int ballVal)
-        {
-            LogicLayer.CreateBallsList(ballVal);
+    
 
-            for (int i = LogicLayer.GetCounter - ballVal; i < LogicLayer.GetCounter; i++)
-            {
-                Ellipse ellipse = new Ellipse { Width = LogicLayer.GetSize(i), Height = LogicLayer.GetSize(i), Fill = Brushes.Black };
-                Canvas.SetLeft(ellipse, LogicLayer.GetX(i));
-                Canvas.SetTop(ellipse, LogicLayer.GetY(i));
-                ellipseCollection.Add(ellipse);
-                Canvas.Children.Add(ellipse);
-            }
-            LogicLayer.Start();
-
-        }
-
-        public override void Move()
-        {
-            for (int i = 0; i < LogicLayer.GetCounter; i++)
-            {
-                Canvas.SetLeft(ellipseCollection[i], LogicLayer.GetX(i));
-                Canvas.SetTop(ellipseCollection[i], LogicLayer.GetY(i));
-            }
-            for (int i = LogicLayer.GetCounter; i < ellipseCollection.Count; i++)
-            {
-                Canvas.Children.Remove(ellipseCollection[ellipseCollection.Count - 1]);
-                ellipseCollection.Remove(ellipseCollection[ellipseCollection.Count - 1]);
-            }
-        }
 
         public override void Stop()
         {
