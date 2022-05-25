8c572e5b33387945b91a8f93e619f1073a4c1a01
Zychu10 <s.zych10@o2.pl>
2022-05-25 23:14:33 +0200

    Tests work clearly

--- Project/LogicTest/LogicTests.cs
+++ Project/LogicTest/LogicTests.cs
@@ -1,53 +1,56 @@
 using Logic;
-using Moq;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-
-namespace LogicTest
+namespace TestLogic
 {
-    /*[TestClass]
-    public class LogicTests
+    [TestClass]
+    public class CreationTest
     {
-        private LogicAbstractAPI LogicAPI;
-        private Mock<TimerApi> timer;
+
+        private LogicAbstractApi LApi;
+
+
+        [TestMethod]
+        public void testCreateApi()
+        {
+            LApi = LogicAbstractApi.CreateApi(800, 600);
+            Assert.IsNotNull(LApi);
+        }
 
         [TestMethod]
         public void getCount()
         {
-            timer = new Mock<TimerApi>();
-            LogicAPI = LogicAbstractAPI.CreateApi(900, 900, timer.Object);
-            LogicAPI.CreateBallsList(8);
-            Assert.AreEqual(8, LogicAPI.GetCounter);
-            LogicAPI.CreateBallsList(-1);
-            Assert.AreEqual(7, LogicAPI.GetCounter);
-            LogicAPI.CreateBallsList(-9);
-            Assert.AreEqual(0, LogicAPI.GetCounter);
+            LApi = LogicAbstractApi.CreateApi(800, 600);
+            Assert.AreEqual(800, LApi.Width);
+            Assert.AreEqual(600, LApi.Height);
+            LApi.CreateBalls(5);
+            Assert.AreEqual(5, LApi.GetCount);
+            LApi.CreateBalls(-3);
+            Assert.AreEqual(2, LApi.GetCount);
+            LApi.CreateBalls(-3);
+            Assert.AreEqual(0, LApi.GetCount);
         }
 
         [TestMethod]
-        public void getRadiusInterval()
+        public void testWallCollision()
         {
-            timer = new Mock<TimerApi>();
-            LogicAPI = LogicAbstractAPI.CreateApi(300, 450, timer.Object);
-            LogicAPI.CreateBallsList(1);
-            int radius = LogicAPI.GetSize(0);
-            Assert.IsTrue(radius >= 10);
-            Assert.IsTrue(radius <= 70);
+            LApi = LogicAbstractApi.CreateApi(800, 600);
+            LApi.CreateBalls(1);
+            LApi.GetBall(0).VelocityX = 5;
+            LApi.GetBall(0).X = 790;
+            Assert.AreNotEqual(5, LApi.GetBall(0).VelocityX);
+            LApi.GetBall(0).VelocityX = -3;
+            LApi.GetBall(0).X = -3;
+            Assert.AreNotEqual(-3, LApi.GetBall(0).VelocityX);
+            LApi.GetBall(0).VelocityY = -7;
+            LApi.GetBall(0).Y = -2;
+            Assert.AreNotEqual(-7, LApi.GetBall(0).VelocityY);
+            LApi.GetBall(0).VelocityY = 7;
+            LApi.GetBall(0).Y = 607;
+            Assert.AreNotEqual(7, LApi.GetBall(0).VelocityY);
+
         }
-        [TestMethod]
-        public void getValidCoordinates()
-        {
-            timer = new Mock<TimerApi>();
-            LogicAPI = LogicAbstractAPI.CreateApi(1000, 200, timer.Object);
-            Assert.AreEqual(1000, LogicAPI.Width);
-            Assert.AreEqual(200, LogicAPI.Height);
-            LogicAPI.CreateBallsList(1);
-            int radius = LogicAPI.GetSize(0);
-            int x = LogicAPI.GetX(0);
-            int y = LogicAPI.GetY(0);
-            Assert.IsTrue(x >= radius);
-            Assert.IsTrue(y >= radius);
-            Assert.IsTrue(x <= LogicAPI.Width - radius);
-            Assert.IsTrue(y <= LogicAPI.Height - radius);
-        }
-    }*/
+
+
+
+    }
 }
